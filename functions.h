b20d6f381103a749d9e85f9c692986f230d665d9
anryyang <anryyang@gmail.com>
2016-10-22 19:42:53 +0800

    add

---
+++ functions.h
@@ -0,0 +1,12 @@
+/*************************************************************************
+    > File Name: functions.h
+    > Author: anryyang
+    > Mail: anryyang@gmail.com 
+    > Created Time: Sat Oct 22 19:38:16 2016
+ ************************************************************************/
+
+#include "graph.h"
+
+void greedy(const Graph &graph, Users &users);
+double get_overall_opinion(const Graph &graph, Users &users);
+double get_my_overall_opinion(const Graph &graph, Users &users);
