e80ff97aaa15418bc8718ba5d026c947a0e2a776
anryyang <anryyang@gmail.com>
2016-10-22 17:17:57 +0800

    add

--- file_2_graph.cpp
+++ file_2_graph.cpp
@@ -6,11 +6,24 @@
  ************************************************************************/
 
 #include "file_2_graph.h"
+#include <stdio.h>  
+#include <sys/time.h>
+#include <time.h> 
 
 FileToGraph::~FileToGraph(){}
 
 void FileToGraph::convert(){
+    // ifstream infile(file_name);
 
+    // string line;
+    // while (std::getline(infile, line))
+    // {
+    //     std::istringstream iss(line);
+    //     int a, b;
+    //     if (!(iss >> a >> b)) { break; } // error
+
+    //     // process pair (a,b)
+    // }    
 }
 
 void FileToGraph::get_graph(Graph &g){
@@ -22,11 +35,30 @@ void FileToGraph::get_users(Users &u){
 }
 
 double FileToGraph::get_weight(){
-    return 0;
+    struct timeval t_val;
+    gettimeofday(&t_val, NULL);
+
+    srand((unsigned)t_val.tv_usec);
+    double weight = rand()/(double)(RAND_MAX);//(rand()%1000)*0.01;
+    return weight;
 }
 
 double FileToGraph::get_opinion(){
-    return 1;
+    struct timeval t_val;
+    gettimeofday(&t_val, NULL);
+
+    srand((unsigned)t_val.tv_usec);
+    double opinion = rand()/(double)(RAND_MAX);//(rand()%1000)*0.01;
+    return opinion;
+}
+
+int main(){
+    FileToGraph f2g("test");
+
+    for(int i=0;i<100;i++){
+        cout<<f2g.get_weight()<<" "<<f2g.get_opinion()<<endl;
+    }
+    return 0;
 }
 
 
